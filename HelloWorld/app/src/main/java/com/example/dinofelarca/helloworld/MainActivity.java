468364986e9e63d823592b599c80ce93ebb72596
Doni <dinof98@gmail.com>
2018-01-19 14:06:11 +0100

    timer

--- HelloWorld/app/src/main/java/com/example/dinofelarca/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/dinofelarca/helloworld/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dinofelarca.helloworld;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,9 +10,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv_clicks;
+    TextView tv_clicks, tv_time;
     Button b_click, b_reset;
 
+    CountDownTimer timer;
+    int time = 10;
     int clicks  = 0;
 
     @Override
@@ -20,10 +23,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tv_clicks = (TextView) findViewById(R.id.tv_clicks);
+        tv_time= (TextView) findViewById(R.id.tv_time);
         b_click = (Button) findViewById(R.id.b_click);
         b_reset = (Button) findViewById(R.id.b_reset);
 
-        b_click.setEnabled(true);
+        b_click.setEnabled(false);
+
+        timer = new CountDownTimer(10000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                time--;
+                tv_time.setText("Time: " + time);
+            }
+
+            @Override
+            public void onFinish() {
+                b_click.setEnabled(false);
+                b_reset.setEnabled(true);
+            }
+        };
 
         b_click.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -40,6 +58,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                time = 10;
+                b_click.setEnabled(true);
+                b_reset.setEnabled(false);
+                timer.start();
                 clicks = 0;
                 tv_clicks.setText("Clicks: 0");
 
