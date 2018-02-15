6099e6984fe02fb97f8df0552dcdf4f07df2098a
xDoni <dinof98@gmail.com>
2018-02-16 00:39:23 +0100

    time

--- StarCollector/app/src/main/java/com/example/dinofelarca/starcollector/MainActivity.java
+++ StarCollector/app/src/main/java/com/example/dinofelarca/starcollector/MainActivity.java
@@ -7,6 +7,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.Image;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +17,7 @@ import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.animation.ValueAnimator;
 import android.view.animation.LinearInterpolator;
@@ -33,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
     private Animation.AnimationListener aListener;
     private static Button btn;
     static Boolean isUp = true;
+    private TextView time;
+
+    int timee = 25;
+    CountDownTimer countDown;
 
 
     @Override
@@ -41,13 +47,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
+        time = (TextView) findViewById(R.id.time);
         star = (ImageView) findViewById(R.id.star);
         ship = (ImageView) findViewById(R.id.ship);
         relLay = (RelativeLayout) findViewById(R.id.layout);
 
         SM = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         gSensor = SM.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        countDown = new CountDownTimer(25000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timee--;
+                time.setText("" + timee);
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
 
 
         if (gSensor == null) {
@@ -98,6 +117,9 @@ public class MainActivity extends AppCompatActivity {
                     isUp = true;
                     star.startAnimation(MainActivity.getVerticalSlideAnimation(relLay.getHeight(),0,6000,0));
                 }
+
+                countDown.start();
+                btn.setEnabled(false);
             }
         });
 
